7f5c2859b1441aa2824848151b1083a53f9950f4
vaishnavi rajput <vaishnavirajput335@gmail.com>
2025-05-27 15:13:45 +0530

    install bcryptjs

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,7 +1,7 @@
 const hisaabModel = require("../models/hisaab-model");
 
 const userModel = require("../models/user-model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 module.exports.landingPageControllers = function (req, res) {
